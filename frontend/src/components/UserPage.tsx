402b331dce6c0d5f2071c871bb874eab110ffb3f
vishaljak <vishaljakhar60@yahoo.com>
2024-06-21 23:15:58 +0400

    show api call status using colors and icons

--- frontend/src/components/UserPage.tsx
+++ frontend/src/components/UserPage.tsx
@@ -1,3 +1,4 @@
+import { CheckBadgeIcon, XCircleIcon } from "@heroicons/react/20/solid";
 import {
   useCreateObject,
   useDeleteObject,
@@ -7,20 +8,32 @@ import {
 import { CustomObject } from "../types/CustomObject";
 
 export function UserPage() {
-  const { data: customObject, isFetching } = useReadObject();
-  const { mutateAsync: updateObject, isPending: isUpdating } =
-    useUpdateObject();
-  const { mutateAsync: createObject, isPending: isCreating } =
-    useCreateObject();
-  const { mutateAsync: deleteObject, isPending: isDeleting } =
-    useDeleteObject();
+  const { data: customObject, isFetching, isSuccess } = useReadObject();
+  const {
+    mutateAsync: updateObject,
+    isPending: isUpdating,
+    isError: isUpdateError,
+    isSuccess: isUpdateSuccess,
+  } = useUpdateObject();
+  const {
+    mutateAsync: createObject,
+    isPending: isCreating,
+    isError: isCreateError,
+    isSuccess: isCreateSuccess,
+  } = useCreateObject();
+  const {
+    mutateAsync: deleteObject,
+    isPending: isDeleting,
+    isError: isDeleteError,
+    isSuccess: isDeleteSuccess,
+  } = useDeleteObject();
 
   return (
     <div className="p-4 h-screen flex flex-col justify-evenly border border-black">
       {customObject === undefined || isFetching ? (
         <div className="text-center text-xl text-gray-600">Loading...</div>
       ) : (
-        <ShowObject object={customObject} />
+        <ShowObject object={customObject} isSuccess={isSuccess} />
       )}
       <div className="flex flex-col space-y-4">
         <Button
@@ -33,6 +46,8 @@ export function UserPage() {
             })
           }
           loading={isUpdating}
+          error={isUpdateError}
+          isSuccess={isUpdateSuccess}
         />
         <Button
           title="Create"
@@ -44,11 +59,15 @@ export function UserPage() {
             })
           }
           loading={isCreating}
+          error={isCreateError}
+          isSuccess={isCreateSuccess}
         />
         <Button
           title="Delete"
           onClick={() => deleteObject(1)}
           loading={isDeleting}
+          error={isDeleteError}
+          isSuccess={isDeleteSuccess}
         />
       </div>
     </div>
@@ -58,13 +77,23 @@ export function UserPage() {
 type ButtonProps = {
   title: string;
   onClick: () => void;
-  loading?: boolean;
+  loading: boolean;
+  error: boolean;
+  isSuccess: boolean;
 };
 
-function Button({ title, onClick, loading = false }: ButtonProps) {
+function Button({
+  title,
+  onClick,
+  loading = false,
+  error,
+  isSuccess,
+}: ButtonProps) {
   return (
     <button
-      className="p-2 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300"
+      className={`p-2 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300 ${
+        error && "border border-red-500"
+      } ${isSuccess && "border-2 border-green-500"}`}
       onClick={onClick}
       disabled={loading}
     >
@@ -73,12 +102,29 @@ function Button({ title, onClick, loading = false }: ButtonProps) {
   );
 }
 
-function ShowObject({ object }: { object: CustomObject }) {
+function ShowObject({
+  object,
+  isSuccess,
+}: {
+  object: CustomObject;
+  isSuccess: boolean;
+}) {
   return (
-    <div className="border border-gray-400 rounded-md p-4 text-center">
-      <span className="text-xl">#{object.id}&nbsp;</span>
-      <span className="font-semibold text-2xl">{object.title}</span>
-      <div className="text-gray-700">{object.description}</div>
+    <div className="flex flex-row justify-between items-center border border-gray-400 rounded-md p-4">
+      <div className="flex flex-col">
+        <div className="flex flex-row gap-1 items-center">
+          <span className="text-xl">#{object.id}</span>
+          <span className="font-semibold text-2xl">{object.title}</span>
+        </div>
+        <div className="text-gray-700">{object.description}</div>
+      </div>
+      <div>
+        {isSuccess ? (
+          <CheckBadgeIcon className="text-green-500 w-12 h-12" />
+        ) : (
+          <XCircleIcon className="text-red-500 w-12 h-12" />
+        )}
+      </div>
     </div>
   );
 }
